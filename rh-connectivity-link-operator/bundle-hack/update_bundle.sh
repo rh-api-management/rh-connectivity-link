1c2f523ace4e9783bfdfdd624222aeb15964e47d
Tommy Condon <tcondon@redhat.com>
2025-05-01 14:47:48 +0100

    Update connectivity link operator update_bundle.sh script

--- rh-connectivity-link-operator/bundle-hack/update_bundle.sh
+++ rh-connectivity-link-operator/bundle-hack/update_bundle.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
-# enables strict mode: `-e` fails if error, `-u` checks variable references, `-o pipefail`: prevents errors in a pipeline from being masked
-set -euo pipefail
+export CONNECTIVITY_LINK_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/api-management-tenant/rh-connectivity-link-operator@sha256:71e63ca10b944fbfd170ccb659f3d600e7a748347ae20e4dc0f7261d03e5ede5"
+export CSV_FILE=/manifests/kuadrant-operator.clusterserviceversion.yaml
 
-#TODO: Add other component image build pullspecs once created
-#export APICAST_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/api-management-tenant/apicast-gateway@sha256:de202ee9c78ae42a8a315563f8257295b4c97402dae72adad93375cdb2196ed7"
-export OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/api-management-tenant/rh-connectivity-link-operator@sha256:30784d3f5d9afc8d5f62a22327796e58914b012b90d5a123d6d658db5a590646"
+export DESCRIPTION=$(cat DESCRIPTION)
 
-export CSV_FILE=/manifests/apicast-operator.clusterserviceversion.yaml
+export ICON=$(cat ICON)
 
-sed -i -e "s|quay.io/3scale/apicast-operator:master|\"${OPERATOR_IMAGE_PULLSPEC}\"|g" "${CSV_FILE}"
-sed -i -e "s|quay.io/3scale/apicast:latest|\"${APICAST_IMAGE_PULLSPEC}\"|g" "${CSV_FILE}"
-
-# metadata/dependencies.yaml for 1.0.1, authorino-operator, limitador-operator and dns-operator dependency versions
-export DEPENDENCIES_RESOURCE=/metadata/dependencies.yaml
-${YQ} eval '.dependencies.[0].value.version = 0.0.0' -i $DEPENDENCIES_RESOURCE
-${YQ} eval '.dependencies.[1].value.version = 0.0.0' -i $DEPENDENCIES_RESOURCE
-${YQ} eval '.dependencies.[2].value.version = 0.0.0' -i $DEPENDENCIES_RESOURCE
-
-export AMD64_BUILT=$(skopeo inspect --raw docker://${OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="amd64")')
-export ARM64_BUILT=$(skopeo inspect --raw docker://${OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="arm64")')
-export PPC64LE_BUILT=$(skopeo inspect --raw docker://${OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="ppc64le")')
-export S390X_BUILT=$(skopeo inspect --raw docker://${OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="s390x")')
+sed -i -e "s|quay.io/kuadrant/kuadrant-operator:.*|\"${CONNECTIVITY_LINK_OPERATOR_IMAGE_PULLSPEC}\"|g" \
+	"${CSV_FILE}"
 
 export EPOC_TIMESTAMP=$(date +%s)
 # time for some direct modifications to the csv
@@ -48,51 +35,31 @@ def dump_manifest(pathn, manifest):
    return
 timestamp = int(os.getenv('EPOC_TIMESTAMP'))
 datetime_time = datetime.fromtimestamp(timestamp)
-csv_manifest = load_manifest(os.getenv('CSV_FILE'))
+rhcl_operator_csv = load_manifest(os.getenv('CSV_FILE'))
 # Add arch and os support labels
-csv_manifest['metadata']['labels'] = csv_manifest['metadata'].get('labels', {})
-if os.getenv('AMD64_BUILT'):
-	csv_manifest['metadata']['labels']['operatorframework.io/arch.amd64'] = 'supported'
-if os.getenv('ARM64_BUILT'):
-	csv_manifest['metadata']['labels']['operatorframework.io/arch.arm64'] = 'supported'
-if os.getenv('PPC64LE_BUILT'):
-	csv_manifest['metadata']['labels']['operatorframework.io/arch.ppc64le'] = 'supported'
-if os.getenv('S390X_BUILT'):
-	csv_manifest['metadata']['labels']['operatorframework.io/arch.s390x'] = 'supported'
-csv_manifest['metadata']['labels']['operatorframework.io/os.linux'] = 'supported'
-csv_manifest['metadata']['annotations']['createdAt'] = datetime_time.strftime('%d %b %Y, %H:%M')
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/disconnected'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/fips-compliant'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/proxy-aware'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/tls-profiles'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/token-auth-aws'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/token-auth-azure'] = 'false'
-csv_manifest['metadata']['annotations']['features.operators.openshift.io/token-auth-gcp'] = 'false'
-# Ensure that other annotations are accurate
-csv_manifest['metadata']['annotations']['repository'] = 'https://github.com/3scale/apicast-operator'
-csv_manifest['metadata']['annotations']['containerImage'] = os.getenv('OPERATOR_IMAGE_PULLSPEC', '')
+rhcl_operator_csv['metadata']['labels'] = rhcl_operator_csv['metadata'].get('labels', {})
+rhcl_operator_csv['metadata']['labels']['operatorframework.io/os.linux'] = 'supported'
+# Ensure that the created timestamp is current
+rhcl_operator_csv['metadata']['annotations']['createdAt'] = datetime_time.strftime('%d %b %Y, %H:%M')
+# Add annotations for the openshift operator features
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/disconnected'] = 'true'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/fips-compliant'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/proxy-aware'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/tls-profiles'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-aws'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-azure'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-gcp'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/cnf'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/cni'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['features.operators.openshift.io/csi'] = 'false'
+rhcl_operator_csv['metadata']['annotations']['operators.openshift.io/valid-subscription'] = '[]'
+rhcl_operator_csv['metadata']['annotations']['operators.openshift.io/valid-subscription'] = '[]'
 
-__dir = os.path.dirname(os.path.abspath(__file__))
+# Add description & icon
+rhcl_operator_csv['metadata']['annotations']['description'] = os.getenv('DESCRIPTION')
+rhcl_operator_csv['spec']['icon'][0]['base64data'] = os.getenv('ICON')
 
-# Ensure that any parameters are properly defined in the spec if you do not want to
-# put them in the CSV itself
-with open(f"{__dir}/DESCRIPTION", "r") as desc_file:
-    description = desc_file.read()
-
-with open(f"{__dir}/ICON", "r") as icon_file:
-    icon_data = icon_file.read()
-
-csv_manifest['spec']['description'] = description
-csv_manifest['spec']['icon'][0]['base64data'] = icon_data
-
-
-# Make sure that our latest nudged references are properly configured in the spec.relatedImages
-# NOTE: the names should be unique
-csv_manifest['spec']['relatedImages'] = [
-   {'name': 'connectivity-link-operator', 'image': os.getenv('OPERATOR_IMAGE_PULLSPEC')}
-]
-
-dump_manifest(os.getenv('CSV_FILE'), csv_manifest)
+dump_manifest(os.getenv('CSV_FILE'), rhcl_operator_csv)
 CSV_UPDATE
 
 cat $CSV_FILE
